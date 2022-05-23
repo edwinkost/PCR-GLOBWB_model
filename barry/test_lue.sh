370539a26c088ed1749d16a371cde21299e69ea5
vanjaarsveldbarry <vanjaarsveldbarry@gmail.com>
2022-05-23 13:28:13 +0200

    test

--- barry/test_lue.sh
+++ barry/test_lue.sh
@@ -72,38 +72,7 @@ function run_on_snellius_partition()
     # - ntasks: $nr_cluster_nodes x $nr_numa_nodes
     # - cpus-per-task: 16
 
-    nr_numa_nodes_pe#!/usr/bin/env bash
-set -e
-
-
-# Change these for your specific setup.
-
-#~ # In Kor's development environment:
-#~ lue_install_prefix="$LUE_OBJECTS"
-#~ hpx_install_prefix="$LUE_OBJECTS/_deps/hpx-build"
-#~
-#~ # On a LUE user's runtime environment:
-#~ lue_install_prefix="prefix/of/wherever/you/installed/lue"
-#~ hpx_install_prefix="$lue_install_prefix"
-
-# edwin
-lue_install_prefix="/home/edwin/opt/lue"
-hpx_install_prefix="$lue_install_prefix"
-
-# A tcn node on snellius:
-# - 2 packages / node
-# - 4 NUMA nodes / package → 8 NUMA nodes
-# - 16 CPU cores / NUMA node → 128 CPU cores
-
-
-#~ export PYTHONPATH=$lue_install_prefix/lib/python3.9
-export PYTHONPATH=$lue_install_prefix/lib64/python3.9
-export LD_PRELOAD=$EBROOTGPERFTOOLS/lib/libtcmalloc_minimal.so.4
-
-
-function run_on_snellius_node()
-
-r_node=8
+    nr_numa_nodes_per_node=8
     nr_cpus_per_task=16
     nr_localities=$(expr $nr_cluster_nodes \* $nr_numa_nodes_per_node)
 
