34783dcfd7ee9bfef24941dbd3b667582fa50ebd
vanjaarsveldbarry <vanjaarsveldbarry@gmail.com>
2022-05-30 12:50:23 +0200

    m

--- barry/hamonETPfunctions_lue.py
+++ barry/hamonETPfunctions_lue.py
@@ -13,59 +13,32 @@ import math
 
 
 @lfr.runtime_scope
-def hello_lue(array_shape,partition_shape):
+def lue_array(array_shape,partition_shape):
     airT = lfr.create_array(array_shape=array_shape, partition_shape=partition_shape,dtype=np.dtype(np.float32), fill_value=5.5)
     airT = (lfr.uniform(airT, -10, 10) * airT) / airT
-    test = lfr.where(airT >= 0.0)
-    print(lfr.to_numpy(airT))
-
-
-#satPressure
-
-    # airT_new = 0.61078 * lfr.exp (17.26939 * airT / (airT + 237.3))
-    # print(lfr.to_numpy(airT_new))
-
-    # print(array)
+    return print(lfr.to_numpy(airT))
 
 
 
 
-# def dayLength(doy,lat):
-#     """ daylength fraction of day  """
-#     lat = lat * pcr.scalar(math.pi) /  180.0
-#     M_PI_2 = pcr.spatial(pcr.scalar(math.pi / 2.0))
-#     dec = pcr.sin( (6.224111 + 0.017202  * doy) *  180. / math.pi)
-#     dec = pcr.scalar(0.39785 * pcr.sin ((4.868961 + .017203 *  doy + 0.033446 * pcr.sin (dec*   180 / math.pi)) *  180 / math.pi))
-#     dec = pcr.scalar(pcr.asin(dec))
-#     lat = pcr.ifthenelse(pcr.abs(lat) > M_PI_2, (M_PI_2 - pcr.scalar(0.01)) * pcr.ifthenelse(lat > 0,  pcr.scalar(1.0), pcr.scalar(-1.0))  ,lat )
-#     arg = pcr.tan(dec ) *  pcr.tan(lat * 180.0 / math.pi  ) * -1.0
-#     h = pcr.scalar( pcr.acos(arg ) )
-#     h = h / 180. * math.pi
-#     h = pcr.ifthenelse(arg > 1.0, 0.0,h) # /* sun stays below horizon */
-#     h = pcr.ifthenelse(arg <  -1.0 ,math.pi,h) # /* sun stays above horizon */
-#     return (h /  math.pi)
 
-
-def satPressure ( airT):
-    """ calculates saturated vp from airt temperature Murray (1967) """
-    # airT      - air temperature [degree C] */
-    satPressure = pcr.ifthenelse(airT >= 0.0 , 0.61078 * pcr.exp (17.26939 * airT / (airT + 237.3)) ,\
-        0.61078 * pcr.exp (21.87456 * airT / (airT + 265.5)))
-    return satPressure
+# def satPressure (airT):
+#     """ calculates saturated vp from airt temperature Murray (1967) """
+#     # airT      - air temperature [degree C] */
+#     satPressure = lfr.greater_than_equal_to()
 #
-# def HamonPotET(airT,doy,lat):
-#     # Note that all following functions use trigonometric function in degres.
-#     reset_globaloption_to_radians = False
-#     if float(pcr.sin(90.0)) < 1.0:
-#         pcr.setglobaloption("degrees")
-#         reset_globaloption_to_radians = True
-#     rhoSat =  2.167 * satPressure (airT) / (airT + 273.15)
-#     dayLen = dayLength(doy,lat)
-#     pet     = 165.1 * 2.0 * dayLen * rhoSat # // 2 * DAYLEN = daylength as frac
-#     pet = pet / 1000 # in meters!
-#     if reset_globaloption_to_radians: pcr.setglobaloption("radians")
-#
-#     return pet
+#     satPressure = pcr.ifthenelse(airT >= 0.0 , 0.61078 * pcr.exp (17.26939 * airT / (airT + 237.3)) ,\
+#         0.61078 * pcr.exp (21.87456 * airT / (airT + 265.5)))
+#     return satPressure
+
+
+# def satPressure (airT):
+#     """ calculates saturated vp from airt temperature Murray (1967) """
+#     # airT      - air temperature [degree C] */
+#     satPressure = pcr.ifthenelse(airT >= 0.0 , 0.61078 * pcr.exp (17.26939 * airT / (airT + 237.3)) ,\
+#         0.61078 * pcr.exp (21.87456 * airT / (airT + 265.5)))
+#     return satPressure
+
 
 
 
