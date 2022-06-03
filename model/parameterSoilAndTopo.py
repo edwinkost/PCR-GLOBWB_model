c6ae4608b5275ef7ff565840b4cbcbf5fc82f49d
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2022-06-03 15:00:51 +0200

    developing with lue

--- model/parameterSoilAndTopo.py
+++ model/parameterSoilAndTopo.py
@@ -25,13 +25,13 @@
 import os
 
 # ~ import pcraster as pcr
-import luepcr as pcr
-print("LUE is USED.")
-# ~ try:
-    # ~ import luepcr as pcr
-    # ~ print("LUE is USED.")
-# ~ except:
-    # ~ import pcraster as pcr		
+# ~ import luepcr as pcr
+# ~ print("LUE is USED.")
+try:
+    import luepcr as pcr
+    print("LUE is USED.")
+except:
+    import pcraster as pcr		
 
 import virtualOS as vos
 
