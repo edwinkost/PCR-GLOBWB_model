d30ecac336ad305bc80df05a3453d6705cc4b59f
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2022-06-03 13:54:23 +0200

    developing with lue

--- model/evaporation/hamonETPFunctions.py
+++ model/evaporation/hamonETPFunctions.py
@@ -22,6 +22,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import sys
+
 # ~ import pcraster as pcr
 sys.path.append("..")
 import luepcr as pcr
