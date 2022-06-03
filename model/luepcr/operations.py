130eccb6e3d09c8531c9f32bd783bd9f2814fbe2
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2022-06-03 09:34:21 +0200

    developing with lue

--- model/luepcr/operations.py
+++ model/luepcr/operations.py
@@ -81,6 +81,8 @@ def ifthenelse(arg1, arg2, arg3):
 def cover(arg1, arg2):
     return lfr.where(lfr.valid(arg1), arg1, arg2)
 
+def ldd
+    return lfr.d8_flow_direction(arg)
 
 # Mimic operations not in LUE
 
