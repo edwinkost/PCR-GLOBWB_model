7392e2421dbb9f76dc04ccd769e2fb1f6d43708d
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2022-06-07 13:13:54 +0200

    minize input

--- model/luepcr/operations.py
+++ model/luepcr/operations.py
@@ -211,7 +211,7 @@ def max(arg1, arg2):
     print("Note 'pcraster.max' function is PARTIALLY implemented on LUE yet.")
     # ~ print(arg1)
     # ~ print(arg2)
-    return lfr.where(lfr.greater_than_equal_to(arg1, arg2), arg1, arg2)
+    # ~ return lfr.where(lfr.greater_than_equal_to(arg1, arg2), arg1, arg2)
     return ifthenelse(arg1 > arg2, arg1, arg2)
 
 
