29a45c0a256a2635ab9843db402c88a578c64970
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2022-06-16 14:53:42 +0200

    do reporting

--- model/deterministic_runner.py
+++ model/deterministic_runner.py
@@ -57,7 +57,7 @@ class DeterministicRunner(DynamicModel):
 
         self.modelTime = modelTime        
         self.model = PCRGlobWB(configuration, modelTime, initialState)
-        # ~ self.reporting = Reporting(configuration, self.model, modelTime)
+        self.reporting = Reporting(configuration, self.model, modelTime)
         
     def initial(self): 
         pass
