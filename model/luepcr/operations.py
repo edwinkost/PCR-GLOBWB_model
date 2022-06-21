3dc4c9ad4e41696595b2ec3db40241198e369eae
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2022-06-21 13:16:57 +0200

    demo for oliver

--- model/luepcr/operations.py
+++ model/luepcr/operations.py
@@ -129,9 +129,9 @@ def cover(arg1, arg2, check_with_aguila = False):
         valid_check = lfr.valid(arg1)
         #~ aguila(valid_check)
         print("after cover")
-        spatial_arg2 = lfr.create_array(array_shape, partition_shape, np.dtype(np.float32), fill_value = arg2)
+        check_arg2 = lfr.create_array(array_shape, partition_shape, np.dtype(np.float32), fill_value = arg2)
         print(arg2)
-        check = lfr.where(lfr.valid(arg1), arg1, spatial_arg2)
+        check = lfr.where(lfr.valid(arg1), arg1, check_arg2)
         aguila(check)
     return lfr.where(lfr.valid(arg1), arg1, arg2)
 
