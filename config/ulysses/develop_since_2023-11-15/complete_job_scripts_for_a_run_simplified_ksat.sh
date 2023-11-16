7801ba6e15e7231f4521d1c93e98d592e50b023e
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2023-11-16 23:35:04 +0100

    test job on snellius

--- config/ulysses/develop_since_2023-11-15/complete_job_scripts_for_a_run_simplified_ksat.sh
+++ config/ulysses/develop_since_2023-11-15/complete_job_scripts_for_a_run_simplified_ksat.sh
@@ -17,15 +17,21 @@ MAIN_OUTPUT_DIR="/scratch-shared/edwin/pcrglobwb_ulysses_test_ksat_november_2023
 set -x
 
 # spin up run
-SUB_JOBNAME=${JOBNAME}_spinup_26years_with_1981
+NUM_OF_YEARS_FOR_SPINUP="25"
+SUB_JOBNAME=${JOBNAME}_spinup_with_1981
 SUB_INIFILE=${SPINUP_RUN_INI}
 STA_DATE="1981-01-01"
 END_DATE="1981-12-31"
 INITIAL_FOLD="/projects/0/dfguu/users/edwin/data/pcrglobwb_input_ulysses/initial_conditions/from_runs_created_in_january_2021/"
 INITIAL_DATE="1981-12-31"
 SUB_OUT_DIR=${MAIN_OUTPUT_DIR}/_spinup/with_1981/
+
+# - for testing
+NUM_OF_YEARS_FOR_SPINUP="25"
+STA_DATE="1981-12-29"
+
 # - start the run
-SPINUP=$(sbatch -J "${SUB_JOBNAME}" --export INI_FILE="${SUB_INIFILE}",MAIN_OUTPUT_DIR="${SUB_OUT_DIR}",STARTING_DATE="${STA_DATE}",END_DATE="${END_DATE}",MAIN_INITIAL_STATE_FOLDER="${INITIAL_FOLD}",DATE_FOR_INITIAL_STATES="${INITIAL_DATE}",BASEFLOW_EXPONENT="${BFEXPON}",LOG_10_MULTIPLIER_FOR_KSAT="${LOGKSAT}",NUM_OF_YEARS_FOR_SPINUP="25" job_script_sbatch_pcrglobwb_template.sh | sed 's/Submitted batch job //')
+SPINUP=$(sbatch -J "${SUB_JOBNAME}" --export INI_FILE="${SUB_INIFILE}",MAIN_OUTPUT_DIR="${SUB_OUT_DIR}",STARTING_DATE="${STA_DATE}",END_DATE="${END_DATE}",MAIN_INITIAL_STATE_FOLDER="${INITIAL_FOLD}",DATE_FOR_INITIAL_STATES="${INITIAL_DATE}",BASEFLOW_EXPONENT="${BFEXPON}",LOG_10_MULTIPLIER_FOR_KSAT="${LOGKSAT}",NUM_OF_YEARS_FOR_SPINUP="${NUM_OF_YEARS_FOR_SPINUP}" job_script_sbatch_pcrglobwb_template.sh | sed 's/Submitted batch job //')
 
 
 # run for the period 1981-2019
