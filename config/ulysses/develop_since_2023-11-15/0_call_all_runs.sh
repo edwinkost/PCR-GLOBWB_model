ffc5d0b3aa9fffaecc24e0ba516c023109c4e653
Edwin Sutanudjaja <edwinkost@UU105378>
2023-11-23 14:25:35 +0100

    start the runs with varying recession coefficients.

--- config/ulysses/develop_since_2023-11-15/0_call_all_runs.sh
+++ config/ulysses/develop_since_2023-11-15/0_call_all_runs.sh
@@ -3,10 +3,10 @@
 set -x
 
 bash complete_job_scripts_for_a_run_simplified_recessionCoef.sh "reccf5.00" "0.69897000433"
-bash complete_job_scripts_for_a_run_simplified_recessionCoef.sh "reccf10.0" "1.0"
 bash complete_job_scripts_for_a_run_simplified_recessionCoef.sh "reccf50.0" "1.69897000434"
 bash complete_job_scripts_for_a_run_simplified_recessionCoef.sh "reccfsqrt" "SQUARE_ROOT"
 
-#~ bash complete_job_scripts_for_a_run_simplified_recessionCoef.sh "reccf1.00" "0.0"
+bash complete_job_scripts_for_a_run_simplified_recessionCoef.sh "reccf1.00" "0.0"
+bash complete_job_scripts_for_a_run_simplified_recessionCoef.sh "reccf10.0" "1.0"
 
 set +x
