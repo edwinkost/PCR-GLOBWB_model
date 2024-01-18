ba743832d53ca1e37a69289bf7d2fcb573e84dad
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2024-01-18 16:24:15 +0100

    finalizing the tool for merging netcdf files

--- model/merge_netcdf_files.py
+++ model/merge_netcdf_files.py
@@ -469,6 +469,8 @@ ncores = min(len(netcdfList), max_number_of_cores)
 areas = str(sys.argv[10])
 if areas == "Global":
     areas = ['M%07d'%i for i in range(1,72,1)]
+elif areas == "Global53":
+    areas = ['M%02d'%i for i in range(1,54,1)]
 else:
     areas = list(set(areas.split(",")))
 
