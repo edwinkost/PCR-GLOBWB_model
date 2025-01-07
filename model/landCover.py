ec9ef9ce8929b48f536f3528fa0484ebf6120c6b
Edwin Sutanudjaja <edwinkost@UU105378>
2025-01-07 09:44:42 +0100

    continue debugging

--- model/landCover.py
+++ model/landCover.py
@@ -2767,6 +2767,8 @@ class LandCover(object):
                              self.kUnsatLow*self.parameters.kUnsatAtFieldCapLow))
                                                                             # original Rens's line:
                                                                             #    P2_L[TYPE] = min(KTHEFF2,sqrt(KTHEFF2*KTHEFF2_FC[TYPE]))*Duration*timeslice()
+            vos.plot_variable_for_lue(self.percLow, "percLow_1_lc")
+
             
             # - capillary rise to storUpp from storLow
             self.capRiseUpp = \
