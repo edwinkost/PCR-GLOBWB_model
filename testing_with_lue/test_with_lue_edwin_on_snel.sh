bed0975f656c40ae389eb3fcf095e6d1a75c1385
Edwin Sutanudjaja <root@puppypc22930>
2025-10-12 17:08:34 +0200

    test on snel - for rhine-meuse, this will be failed

--- testing_with_lue/test_with_lue_edwin_on_snel.sh
+++ testing_with_lue/test_with_lue_edwin_on_snel.sh
@@ -50,12 +50,17 @@ libtcmalloc=$(find $EBROOTGPERFTOOLS -name libtcmalloc_minimal.so.4)
      #~ python /home/sutan101/github/edwinkost/PCR-GLOBWB_model/model/deterministic_runner.py /home/sutan101/github/edwinkost/PCR-GLOBWB_model/config/lue/setup_30min_on_velocity_for_lue.ini debug \
          #~ --hpx:threads=1
 
+#~ LD_PRELOAD=$libtcmalloc \
+#~ LUE_PCRASTER_PROVIDER_NAME=lue \
+#~ LUE_PARTITION_SHAPE="180,180" \
+     #~ python /home/edwin/github/edwinkost/PCR-GLOBWB_model/model/deterministic_runner.py /home/edwin/github/edwinkost/PCR-GLOBWB_model/config/lue/setup_30min_on_velocity_for_lue_on_snel.ini debug \
+         #~ --hpx:threads=16
+
 LD_PRELOAD=$libtcmalloc \
 LUE_PCRASTER_PROVIDER_NAME=lue \
-LUE_PARTITION_SHAPE="180,180" \
+LUE_PARTITION_SHAPE="13,17" \
      python /home/edwin/github/edwinkost/PCR-GLOBWB_model/model/deterministic_runner.py /home/edwin/github/edwinkost/PCR-GLOBWB_model/config/lue/setup_30min_on_velocity_for_lue_on_snel.ini debug \
-         --hpx:threads=16
-
+         --hpx:threads=1
 
 #~ # Run this script with PCRaster package in environment. This should work:
 #~ # python -c "import pcraster, lue.framework.pcraster_provider"
