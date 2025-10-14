747df2394f30b6e375adcc9ab79621d5cfa1bcd3
Edwin Sutanudjaja <root@puppypc22930>
2025-10-14 14:34:13 +0200

    test on snel

--- testing_with_lue/test_with_lue_edwin_on_snel.sh
+++ testing_with_lue/test_with_lue_edwin_on_snel.sh
@@ -58,7 +58,7 @@ libtcmalloc=$(find $EBROOTGPERFTOOLS -name libtcmalloc_minimal.so.4)
 
 LD_PRELOAD=$libtcmalloc \
 LUE_PCRASTER_PROVIDER_NAME=lue \
-LUE_PARTITION_SHAPE="13,17" \
+#~ LUE_PARTITION_SHAPE="13,17" \
      python /home/edwin/github/edwinkost/PCR-GLOBWB_model/model/deterministic_runner.py /home/edwin/github/edwinkost/PCR-GLOBWB_model/config/lue/setup_30min_on_velocity_for_lue_on_snel.ini debug \
          --hpx:threads=1
 
