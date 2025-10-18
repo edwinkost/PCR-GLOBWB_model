6c6778f65ee49c3bef0853d6c851222e95b77523
Edwin Sutanudjaja <edwinkost@heol>
2025-10-18 14:39:32 +0200

    testing a 30min run with ldd 5 and no reservoirs and no lakes - on snellius

--- model/waterBodies.py
+++ model/waterBodies.py
@@ -165,6 +165,8 @@ class WaterBodies(object):
                 self.fracWat = vos.readPCRmapClone(\
                                self.fracWaterInp+str(year_used)+".map",
                                self.cloneMap,self.tmpDir,self.inputDir)
+            else:
+                self.fracWat = pcr.spatial(pcr.scalar(0.0))                    
         
         self.fracWat = pcr.cover(self.fracWat, pcr.spatial(pcr.scalar(0.0)))
         self.fracWat = pcr.max(0.0, self.fracWat)
@@ -183,7 +185,9 @@ class WaterBodies(object):
             if self.waterBodyIdsInp != "None":
                 self.waterBodyIds = vos.readPCRmapClone(\
                     self.waterBodyIdsInp+str(year_used)+".map",\
-                    self.cloneMap,self.tmpDir,self.inputDir,False,None,True)
+                    self.cloneMap,self.tmpDir, self.inputDir, False, None, True)
+            else:
+                self.waterBodyIds = pcr.spatial(pcr.nominal(-99))                    
         #
         self.waterBodyIds = pcr.ifthen(\
                             pcr.scalar(self.waterBodyIds) > 0.,\
@@ -192,17 +196,39 @@ class WaterBodies(object):
         # water body outlets (correcting outlet positions)
         if "correct_water_body_outlets" in self.iniItems.routingOptions.keys():
 
-            file_for_correct_water_body_outlets = vos.getFullPath(inputPath        = self.iniItems.routingOptions['correct_water_body_outlets'],\
-                                                                  absolutePath     = self.iniItems.globalOptions['inputDir'],\
-                                                                  completeFileName = True)
-            self.waterBodyOut = pcr.readmap(file_for_correct_water_body_outlets)
+            if self.iniItems.routingOptions['correct_water_body_ids'] != "None":
+            
+                file_for_correct_water_body_outlets = vos.getFullPath(inputPath        = self.iniItems.routingOptions['correct_water_body_outlets'],\
+                                                                      absolutePath     = self.iniItems.globalOptions['inputDir'],\
+                                                                      completeFileName = True)
+                self.waterBodyOut = pcr.readmap(file_for_correct_water_body_outlets)
+			    
+                file_for_correct_water_body_ids     = vos.getFullPath(inputPath        = self.iniItems.routingOptions['correct_water_body_ids'],\
+                                                                      absolutePath     = self.iniItems.globalOptions['inputDir'],\
+                                                                      completeFileName = True)
+                self.waterBodyIds = pcr.readmap(file_for_correct_water_body_ids)
+                
+                # ~ pietje
 
             file_for_correct_water_body_ids     = vos.getFullPath(inputPath        = self.iniItems.routingOptions['correct_water_body_ids'],\
                                                                   absolutePath     = self.iniItems.globalOptions['inputDir'],\
                                                                   completeFileName = True)
             self.waterBodyIds = pcr.readmap(file_for_correct_water_body_ids)
             
-            # ~ pietje
+                file_for_correct_water_body_outlets = vos.getFullPath(inputPath        = self.iniItems.routingOptions['correct_water_body_outlets'],\
+                                                                      absolutePath     = self.iniItems.globalOptions['inputDir'],\
+                                                                      completeFileName = True)
+                self.waterBodyOut = pcr.readmap(file_for_correct_water_body_outlets)
+			    
+                file_for_correct_water_body_ids     = vos.getFullPath(inputPath        = self.iniItems.routingOptions['correct_water_body_ids'],\
+                                                                      absolutePath     = self.iniItems.globalOptions['inputDir'],\
+                                                                      completeFileName = True)
+                self.waterBodyIds = pcr.readmap(file_for_correct_water_body_ids)
+                
+                # ~ pietje
+
+            else:
+                pass
             
         else:
         
@@ -287,6 +313,9 @@ class WaterBodies(object):
                 self.waterBodyTyp = vos.readPCRmapClone(
                     self.waterBodyTypInp+str(year_used)+".map",\
                     self.cloneMap,self.tmpDir,self.inputDir,False,None,True)
+            else:
+                # assuming all negative (not being defined and used)
+                self.waterBodyTyp = pcr.nominal(-99)
 
         # excluding wetlands (waterBodyTyp = 0) in all functions related to lakes/reservoirs 
         #
@@ -324,6 +353,9 @@ class WaterBodies(object):
                 self.resMaxCap = 1000. * 1000. * vos.readPCRmapClone(\
                     self.resMaxCapInp+str(year_used)+".map", \
                     self.cloneMap,self.tmpDir,self.inputDir)
+            else:
+                self.resMaxCap = pcr.scalar(0.0)
+
 
         self.resMaxCap = pcr.ifthen(self.resMaxCap > 0.,\
                                     self.resMaxCap)
