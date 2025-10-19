932a8b645613ec48d84daa5df2ca5853a09eba38
Edwin Sutanudjaja <edwinkost@heol>
2025-10-19 08:57:04 +0200

    testing a 30sec run for africa - on snellius

--- testing_with_lue/test_with_lue_for_toine_africa30sec.sh
+++ testing_with_lue/test_with_lue_for_toine_africa30sec.sh
@@ -41,7 +41,7 @@ LD_PRELOAD=$libtcmalloc \
 LUE_PCRASTER_PROVIDER_NAME=lue \
 LUE_PARTITION_SHAPE="9000,9600" \
      python /home/edwin/github/edwinkost/PCR-GLOBWB_model/model/deterministic_runner.py /home/edwin/github/edwinkost/PCR-GLOBWB_model/config/lue/30sec_africa_test.ini debug \
-         --hpx:threads=4
+         --hpx:threads=1
 
 #~ LD_PRELOAD=$libtcmalloc \
 #~ LUE_PCRASTER_PROVIDER_NAME=lue \
