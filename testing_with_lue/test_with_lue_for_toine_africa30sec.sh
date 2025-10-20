d5440d4b36aff0924062297048c5bfd303ad7687
Edwin Sutanudjaja <edwinkost@heol>
2025-10-20 09:01:08 +0200

    try to fix reporting - wait until numpy is ready'

--- testing_with_lue/test_with_lue_for_toine_africa30sec.sh
+++ testing_with_lue/test_with_lue_for_toine_africa30sec.sh
@@ -45,9 +45,15 @@ libtcmalloc=$(find $EBROOTGPERFTOOLS -name libtcmalloc_minimal.so.4)
 
 LD_PRELOAD=$libtcmalloc \
 LUE_PCRASTER_PROVIDER_NAME=lue \
-LUE_PARTITION_SHAPE="300,300" \
+LUE_PARTITION_SHAPE="600,1200" \
      python /home/edwin/github/edwinkost/PCR-GLOBWB_model/model/deterministic_runner.py /home/edwin/github/edwinkost/PCR-GLOBWB_model/config/lue/30sec_africa_test.ini debug \
          --hpx:threads=96
+
+#~ LD_PRELOAD=$libtcmalloc \
+#~ LUE_PCRASTER_PROVIDER_NAME=lue \
+#~ LUE_PARTITION_SHAPE="300,300" \
+     #~ python /home/edwin/github/edwinkost/PCR-GLOBWB_model/model/deterministic_runner.py /home/edwin/github/edwinkost/PCR-GLOBWB_model/config/lue/30sec_africa_test.ini debug \
+         #~ --hpx:threads=96
 
 #~ LD_PRELOAD=$libtcmalloc \
 #~ LUE_PCRASTER_PROVIDER_NAME=lue \
