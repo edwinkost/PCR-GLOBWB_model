5102d89b31fd2fce381cc63f4176ab4edcb4ffa1
Edwin Sutanudjaja <edwinkost@heol>
2025-10-20 08:46:10 +0200

    try to fix reporting - wait until numpy is ready'

--- testing_with_lue/test_with_lue_for_toine.sh
+++ testing_with_lue/test_with_lue_for_toine.sh
@@ -45,7 +45,7 @@ libtcmalloc=$(find $EBROOTGPERFTOOLS -name libtcmalloc_minimal.so.4)
 
 LD_PRELOAD=$libtcmalloc \
 LUE_PCRASTER_PROVIDER_NAME=lue \
-LUE_PARTITION_SHAPE="60,60" \
+LUE_PARTITION_SHAPE="45,60" \
      python /home/edwin/github/edwinkost/PCR-GLOBWB_model/model/deterministic_runner.py /home/edwin/github/edwinkost/PCR-GLOBWB_model/config/lue/setup_30min_snel_test.ini debug \
          --hpx:threads=24
 
