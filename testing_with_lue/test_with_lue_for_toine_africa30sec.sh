0ae0ac9e1344ffd0b1e27bfca5a05299ceed45e4
Edwin Sutanudjaja <root@puppypc22930>
2025-10-21 11:19:43 +0200

    follow Kor's suggestion

--- testing_with_lue/test_with_lue_for_toine_africa30sec.sh
+++ testing_with_lue/test_with_lue_for_toine_africa30sec.sh
@@ -45,9 +45,9 @@ libtcmalloc=$(find $EBROOTGPERFTOOLS -name libtcmalloc_minimal.so.4)
 
 LD_PRELOAD=$libtcmalloc \
 LUE_PCRASTER_PROVIDER_NAME=lue \
-LUE_PARTITION_SHAPE="600,600" \
+LUE_PARTITION_SHAPE="2000,2000" \
      python /home/edwin/github/edwinkost/PCR-GLOBWB_model/model/deterministic_runner.py /home/edwin/github/edwinkost/PCR-GLOBWB_model/config/lue/30sec_africa_vdevelop.ini debug \
-         --hpx:threads=96
+         --hpx:threads=16
 
 
 #~ LD_PRELOAD=$libtcmalloc \
