f6dae5f509cfdd7a87105522cac8670133b32b50
Edwin Sutanudjaja <root@puppypc22930>
2025-10-21 11:53:06 +0200

    finalizing a version for Toine

--- testing_with_lue/test_with_lue_for_toine_africa30sec.sh
+++ testing_with_lue/test_with_lue_for_toine_africa30sec.sh
@@ -37,17 +37,24 @@ module load LUE/development-foss-2024a
 
 libtcmalloc=$(find $EBROOTGPERFTOOLS -name libtcmalloc_minimal.so.4)
 
+LD_PRELOAD=$libtcmalloc \
+LUE_PCRASTER_PROVIDER_NAME=lue \
+LUE_PARTITION_SHAPE="600,600" \
+     python /home/edwin/github/edwinkost/PCR-GLOBWB_model/model/deterministic_runner.py /home/edwin/github/edwinkost/PCR-GLOBWB_model/config/lue/30sec_africa_vdevelop.ini debug \
+         --hpx:threads=96
+
+
 #~ LD_PRELOAD=$libtcmalloc \
 #~ LUE_PCRASTER_PROVIDER_NAME=lue \
 #~ LUE_PARTITION_SHAPE="9000,9600" \
      #~ python /home/edwin/github/edwinkost/PCR-GLOBWB_model/model/deterministic_runner.py /home/edwin/github/edwinkost/PCR-GLOBWB_model/config/lue/30sec_africa_test.ini debug \
          #~ --hpx:threads=24
 
-LD_PRELOAD=$libtcmalloc \
-LUE_PCRASTER_PROVIDER_NAME=lue \
-LUE_PARTITION_SHAPE="2000,3000" \
-     python /home/edwin/github/edwinkost/PCR-GLOBWB_model/model/deterministic_runner.py /home/edwin/github/edwinkost/PCR-GLOBWB_model/config/lue/30sec_africa_vdevelop.ini debug \
-         --hpx:threads=16
+#~ LD_PRELOAD=$libtcmalloc \
+#~ LUE_PCRASTER_PROVIDER_NAME=lue \
+#~ LUE_PARTITION_SHAPE="2000,2000" \
+     #~ python /home/edwin/github/edwinkost/PCR-GLOBWB_model/model/deterministic_runner.py /home/edwin/github/edwinkost/PCR-GLOBWB_model/config/lue/30sec_africa_vdevelop.ini debug \
+         #~ --hpx:threads=16
 
 #~ LD_PRELOAD=$libtcmalloc \
 #~ LUE_PCRASTER_PROVIDER_NAME=lue \
