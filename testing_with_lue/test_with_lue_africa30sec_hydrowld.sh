814ea6176e8c4f43f800d38f1c3827019f563c4c
U-SOLISCOM\Sutan101 <Sutan101@UU105378.soliscom.uu.nl>
2025-12-18 12:21:43 +0100

    test africa

--- testing_with_lue/test_with_lue_africa30sec_hydrowld.sh
+++ testing_with_lue/test_with_lue_africa30sec_hydrowld.sh
@@ -1,7 +1,7 @@
 set -eu
 
 # on snellius
-export MODULEPATH=/home/edwin/.local/easybuild/RHEL9/2024/modulefiles/all/:$MODULEPATH
+export MODULEPATH=/home/hydrowld/.local/easybuild/RHEL9/2024/modulefiles/all/:$MODULEPATH
 module load 2024 
 module load PCRaster/4.4.2-foss-2024a
 module load netcdf4-python/1.7.1.post2-foss-2024a
@@ -15,5 +15,5 @@ libtcmalloc=$(find $EBROOTGPERFTOOLS -name libtcmalloc_minimal.so.4)
 LD_PRELOAD=$libtcmalloc \
 LUE_PCRASTER_PROVIDER_NAME=lue \
 LUE_PARTITION_SHAPE="600,600" \
-     python /home/edwin/github/edwinkost/PCR-GLOBWB_model/model/deterministic_runner.py /home/edwin/github/edwinkost/PCR-GLOBWB_model/config/lue/30sec_africa_vdevelop.ini debug \
+     python /home/hydrowld/github/hydrowldkost/PCR-GLOBWB_model/model/deterministic_runner.py /home/hydrowld/github/hydrowldkost/PCR-GLOBWB_model/config/lue/30sec_africa_vdevelop.ini debug \
          --hpx:threads=96
