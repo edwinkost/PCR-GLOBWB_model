c887f40fefa0f61a47109daa7df54c488b65ad04
U-SOLISCOM\Sutan101 <Sutan101@UU105378.soliscom.uu.nl>
2025-12-27 08:28:28 +0100

    try again with 72 threads

--- testing_with_lue/test_with_lue_africa30sec_hydrowld.sh
+++ testing_with_lue/test_with_lue_africa30sec_hydrowld.sh
@@ -12,11 +12,17 @@ module load LUE/development-foss-2024a
 
 libtcmalloc=$(find $EBROOTGPERFTOOLS -name libtcmalloc_minimal.so.4)
 
+#~ LD_PRELOAD=$libtcmalloc \
+#~ LUE_PCRASTER_PROVIDER_NAME=lue \
+#~ LUE_PARTITION_SHAPE="600,600" \
+     #~ python /home/hydrowld/github/edwinkost/PCR-GLOBWB_model/model/deterministic_runner.py /home/hydrowld/github/edwinkost/PCR-GLOBWB_model/config/lue/30sec_africa_vdevelop.ini debug \
+         #~ --hpx:threads=96
+
 LD_PRELOAD=$libtcmalloc \
 LUE_PCRASTER_PROVIDER_NAME=lue \
 LUE_PARTITION_SHAPE="600,600" \
      python /home/hydrowld/github/edwinkost/PCR-GLOBWB_model/model/deterministic_runner.py /home/hydrowld/github/edwinkost/PCR-GLOBWB_model/config/lue/30sec_africa_vdevelop.ini debug \
-         --hpx:threads=96
+         --hpx:threads=72
 
 #~ LD_PRELOAD=$libtcmalloc \
 #~ LUE_PCRASTER_PROVIDER_NAME=lue \
