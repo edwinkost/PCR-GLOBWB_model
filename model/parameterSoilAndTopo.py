6b20f623586b1620c374faa89eb4910b65e8f214
Edwin Sutanudjaja <root@puppypc15017.edwin.sandisk>
2026-02-24 17:39:21 +0100

    make it able to read laea system

--- model/parameterSoilAndTopo.py
+++ model/parameterSoilAndTopo.py
@@ -138,6 +138,8 @@ class SoilAndTopoParameters(object):
                     vars(self)[var] = pcr.cover(vars(self)[var],
                                       pcr.windowaverage(vars(self)[var], 1.00))
 
+                pcr.aguila(vars(self)[var])
+                
                 vars(self)[var] = pcr.cover(vars(self)[var], 0.0)
 
         else:
