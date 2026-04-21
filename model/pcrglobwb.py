45275b50f1479573e37c2601e1a6206a3c07d2b9
Edwin Sutanudjaja <edwinkost@heol>
2026-04-21 11:33:50 +0200

    test again

--- model/pcrglobwb.py
+++ model/pcrglobwb.py
@@ -306,10 +306,13 @@ class PCRGlobWB(object):
                     % (totalCellArea/1e6)
             logger.info(msg)
 
-            deltaStorageOneYear = vos.getMapVolume( \
-                                     pcr.ifthen(self.landmask,landWaterStoresAtBeginning) - \
-                                     pcr.ifthen(self.landmask,self.storageAtFirstDay),
-                                     self.routing.cellArea)
+            # TODO: Remove this hack for LUE
+            deltaStorageOneYear = 0.0
+            # ~ deltaStorageOneYear = vos.getMapVolume( \
+                                     # ~ pcr.ifthen(self.landmask,landWaterStoresAtBeginning) - \
+                                     # ~ pcr.ifthen(self.landmask,self.storageAtFirstDay),
+                                     # ~ self.routing.cellArea)
+            
             msg = 'Delta total storage days 1 to %i in %i = %e km3 = %e mm'\
                 % (    int(self._modelTime.doy),\
                        int(self._modelTime.year),\
