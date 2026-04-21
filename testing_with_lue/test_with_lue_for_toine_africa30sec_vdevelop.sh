31f1c90c17624be42302de9a39988a69e9fad24b
Edwin Sutanudjaja <edwinkost@heol>
2026-04-21 11:14:01 +0200

    test again

--- testing_with_lue/test_with_lue_for_toine_africa30sec_vdevelop.sh
+++ testing_with_lue/test_with_lue_for_toine_africa30sec_vdevelop.sh
@@ -10,24 +10,22 @@
 #~ #SBATCH -o %x_out.txt
 #~ #SBATCH -e %x_err.txt
 
-set -eu
-
 # on snellius
 
 #~ # - if you are not "edwin" you have to set the MODULEPATH as the following (so that you will load LUE etc compiled by "edwin")
 #~ export MODULEPATH=/home/edwin/.local/easybuild/RHEL9/2025/modulefiles/all/:$MODULEPATH
 
-# - load NEW LUE etc
-module load 2025 
-module load PCRaster/development-foss-2025a
-module load netcdf4-python/1.7.2-foss-2025a
-module load LUE/development-foss-2025a
+#~ # - load NEW LUE etc
+#~ module load 2025 
+#~ module load PCRaster/development-foss-2025a
+#~ module load netcdf4-python/1.7.2-foss-2025a
+#~ module load LUE/development-foss-2025a
 
-#~ # - load OLD LUE etc
-#~ module load 2024 
-#~ module load PCRaster/4.4.2-foss-2024a
-#~ module load netcdf4-python/1.7.1.post2-foss-2024a
-#~ module load LUE/development-foss-2024a
+# - load OLD LUE etc
+module load 2024 
+module load PCRaster/4.4.2-foss-2024a
+module load netcdf4-python/1.7.1.post2-foss-2024a
+module load LUE/development-foss-2024a
 
 
 #~ # Run this script with LUE package in environment. This should work:
@@ -39,7 +37,8 @@ LD_PRELOAD=$libtcmalloc \
 LUE_PCRASTER_PROVIDER_NAME=lue \
 LUE_PARTITION_SHAPE="600,600" \
      python /home/edwin/github/edwinkost/PCR-GLOBWB_model/model/deterministic_runner.py /home/edwin/github/edwinkost/PCR-GLOBWB_model/config/lue/30sec_africa_v2026develop.ini debug \
-         #~ --hpx:threads=48 \
+         --hpx:threads=48 \
          #~ --hpx:threads=24 \
          --end
-         
+
+       
