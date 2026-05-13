ceebd4858f2c4d8801556a2b5ce4899b0b27ee2c
root <root@puppylinux_vgen>
2026-05-13 14:45:01 +0200

    setup_6min_global_watersis

---
+++ config/watersis/linking_routing_input_files.sh
@@ -0,0 +1,23 @@
+
+TARGET_DIR="/scratch/depfg/sutan101/pcrglobwb_input_watersis/develop/global_6min//"
+cd ${TARGET_DIR}
+
+SOURCE_DIR="/scratch/depfg/sutan101/pcrglobwb_input_ulysses_v202312XX/develop_edwin/"
+
+ln -s ${SOURCE_DIR}/lddsound_06min_version_202007XX_for_ulysses.map
+ln -s ${SOURCE_DIR}/cellarea.map
+ln -s ${SOURCE_DIR}/gradient_channel_parameters_6min_ulysses_v20231130_merged.nc.map
+ln -s ${SOURCE_DIR}/bankfull_depth_channel_parameters_6min_ulysses_v20231130_merged.nc.map
+ln -s ${SOURCE_DIR}/bankfull_width_channel_parameters_6min_ulysses_v20231130_merged.nc.map
+ln -s ${SOURCE_DIR}/cropCoefficientForOpenWater.nc
+ln -s ${SOURCE_DIR}/lakes_and_reservoirs_06min_global_version_july_2020_for_ulysses.nc
+
+
+
+
+
+
+
+
+
+
