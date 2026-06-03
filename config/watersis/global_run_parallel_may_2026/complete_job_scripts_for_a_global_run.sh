85f89e42593875f45bfbf9b14077bce175bb3814
U-SOLISCOM\Sutan101 <Sutan101@UU105378.soliscom.uu.nl>
2026-06-03 12:07:12 +0200

    working on the parallelization of the global run

--- config/watersis/global_run_parallel_may_2026/complete_job_scripts_for_a_global_run.sh
+++ config/watersis/global_run_parallel_may_2026/complete_job_scripts_for_a_global_run.sh
@@ -15,33 +15,33 @@ MAIN_INPUT_DIRECTORY="/scratch/depfg/sutan101/pcrglobwb_input_watersis/develop/g
 
 # spin up run
 NUM_OF_YEARS_FOR_SPINUP="25"
-SUB_JOBNAME=${JOBNAME}_spinup_with_1981
+SUB_JOBNAME=${JOBNAME}_spinup_with_1970
 SUB_INIFILE=${SPINUP_RUN_INI}
-STA_DATE="1981-01-01"
-END_DATE="1981-12-31"
+STA_DATE="1970-01-01"
+END_DATE="1970-12-31"
 INITIAL_FOLD="/scratch/depfg/sutan101/pcrglobwb_input_watersis/develop/global_6min/dummy_initial_conditions/after_spinups_with_1981/"
 INITIAL_DATE="1981-12-31"
 SUB_OUT_DIR=${MAIN_OUTPUT_DIR}/_spinup/with_1981/
 
 # - for testing
 NUM_OF_YEARS_FOR_SPINUP="0"
-STA_DATE="1981-12-29"
+STA_DATE="1970-12-29"
 
 #~ # - for testing with 5 year spinup
 #~ NUM_OF_YEARS_FOR_SPINUP="5"
-#~ STA_DATE="1981-01-01"
+#~ STA_DATE="1970-01-01"
 
 # - start the run
 SPINUP=$(sbatch -J "${SUB_JOBNAME}" --export INI_FILE="${SUB_INIFILE}",MAIN_OUTPUT_DIR="${SUB_OUT_DIR}",STARTING_DATE="${STA_DATE}",END_DATE="${END_DATE}",MAIN_INITIAL_STATE_FOLDER="${INITIAL_FOLD}",DATE_FOR_INITIAL_STATES="${INITIAL_DATE}",NUM_OF_YEARS_FOR_SPINUP="${NUM_OF_YEARS_FOR_SPINUP}",MAIN_INPUT_DIRECTORY="${MAIN_INPUT_DIRECTORY}" job_script_sbatch_pcrglobwb_template.sh | sed 's/Submitted batch job //')
 
 
-# run for the period 1981-2019
-SUB_JOBNAME=${JOBNAME}_1981-2019
+# run for the period 1970-2019
+SUB_JOBNAME=${JOBNAME}_1970-2019
 SUB_INIFILE=${WARMED_RUN_INI}
-STA_DATE="1981-01-01"
+STA_DATE="1970-01-01"
 END_DATE="2019-12-31"
 INITIAL_FOLD=${SUB_OUT_DIR}/global/states/
-INITIAL_DATE="1981-12-31"
+INITIAL_DATE="1970-12-31"
 SUB_OUT_DIR=${MAIN_OUTPUT_DIR}/begin_from_1981/
 # - start the run
 FIRST=$(sbatch --dependency=afterany:${SPINUP} -J "${SUB_JOBNAME}" --export INI_FILE="${SUB_INIFILE}",MAIN_OUTPUT_DIR="${SUB_OUT_DIR}",STARTING_DATE="${STA_DATE}",END_DATE="${END_DATE}",MAIN_INITIAL_STATE_FOLDER="${INITIAL_FOLD}",DATE_FOR_INITIAL_STATES="${INITIAL_DATE}",NUM_OF_YEARS_FOR_SPINUP="0",MAIN_INPUT_DIRECTORY="${MAIN_INPUT_DIRECTORY}" job_script_sbatch_pcrglobwb_template.sh | sed 's/Submitted batch job //')
---
+++ config/watersis/global_run_parallel_may_2026/test_run.sh
@@ -0,0 +1,33 @@
+
+
+INI_FILE="../config/setup_6min_global_watersis_develop.ini"
+
+MAIN_OUTPUT_DIRECTORY="/scratch/depfg/sutan101/watersis_runs_may_2026/test_global_6min_with_watersis_forcing_with_parallel/test_single_clone/"     
+MAIN_INPUT_DIRECTORY="/scratch/depfg/sutan101_new/pcrglobwb_input_watersis/develop/global_6min/"   
+STARTING_DATE="1970-01-01"             
+END_DATE="2019-12-31"                  
+NUM_OF_YEARS_FOR_SPINUP="1"   
+PRECIPITATION_FORCING_FILE="/scratch/depfg/sutan101/data/watersis_forcing/global/v20260514/global_pre_0p1_v20260514.nc"
+TEMPERATURE_FORCING_FILE="/scratch/depfg/sutan101/data/watersis_forcing/global/v20260514/global_tas_day_0p1_v20260514.nc"  
+REF_POT_ET_FORCING_FILE="/scratch/depfg/sutan101/data/watersis_forcing/global/v20260514/global_pet_hargreaves_samani_0p1_v20260514.nc"   
+MAIN_INITIAL_STATE_FOLDER="dummy_initial_conditions/" 
+DATE_FOR_INITIAL_STATES="1999-12-31"   
+
+# go to the script folder
+cd /eejit/home/sutan101/github/edwinkost/PCR-GLOBWB_model/model/
+
+#~ python3 deterministic_runner_ulysses.py ${INI_FILE} debug_parallel ${CLONE_CODE} \
+
+python3 deterministic_runner_ulysses.py ${INI_FILE} debug \
+
+-mod     ${MAIN_OUTPUT_DIRECTORY}      \
+-mid     ${MAIN_INPUT_DIRECTORY}       \
+-sd      ${STARTING_DATE}              \
+-ed      ${END_DATE}                   \
+-noyfsu  ${NUM_OF_YEARS_FOR_SPINUP}    \
+-pff     ${PRECIPITATION_FORCING_FILE} \
+-tff     ${TEMPERATURE_FORCING_FILE}   \
+-rpetff  ${REF_POT_ET_FORCING_FILE}    \
+-misf    ${MAIN_INITIAL_STATE_FOLDER}  \
+-dfis    ${DATE_FOR_INITIAL_STATES}    \
+-end_of_arguments
