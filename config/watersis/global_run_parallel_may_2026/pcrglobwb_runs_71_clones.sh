9784513b562dc18957f66fe6d1c74df22a4823dc
U-SOLISCOM\Sutan101 <Sutan101@UU105378.soliscom.uu.nl>
2026-06-03 14:05:14 +0200

    working on the parallelization of the global run

--- config/watersis/global_run_parallel_may_2026/pcrglobwb_runs_71_clones.sh
+++ config/watersis/global_run_parallel_may_2026/pcrglobwb_runs_71_clones.sh
@@ -12,24 +12,36 @@
 
 set -x
 
-# get the aguments
-INI_FILE=$1                          
-MAIN_OUTPUT_DIRECTORY=$2             
-STARTING_DATE=$3                     
-END_DATE=$4                          
-NUM_OF_YEARS_FOR_SPINUP=$5        
-=MAIN_INITIAL_STATE_FOLDER=$6         
-DATE_FOR_INITIAL_STATES=$7           
-MAIN_INPUT_DIRECTORY=$8
-PRECIPITATION_FORCING_FILE=$8        
-TEMPERATURE_FORCING_FILE=$9          
-REF_POT_ET_FORCING_FILE=${10}           
+INI_FILE="../config/watersis/global_run_parallel_may_2026/setup_6min_global_watersis_develop.ini"
+MAIN_OUTPUT_DIRECTORY="/scratch/depfg/sutan101/watersis_runs_may_2026/test_global_6min_with_watersis_forcing_with_parallel/test_multiple_clones/"     
+STARTING_DATE="1970-01-01"             
+END_DATE="2019-12-31"                  
+NUM_OF_YEARS_FOR_SPINUP=1   
+MAIN_INITIAL_STATE_FOLDER="dummy_initial_conditions/" 
+DATE_FOR_INITIAL_STATES="1999-12-31"   
+MAIN_INPUT_DIRECTORY="/scratch/depfg/sutan101_new/pcrglobwb_input_watersis/develop/global_6min/"   
+PRECIPITATION_FORCING_FILE="/scratch/depfg/sutan101/data/watersis_forcing/global/v20260514/global_pre_0p1_v20260514.nc"
+TEMPERATURE_FORCING_FILE="/scratch/depfg/sutan101/data/watersis_forcing/global/v20260514/global_tas_day_0p1_v20260514.nc"  
+REF_POT_ET_FORCING_FILE="/scratch/depfg/sutan101/data/watersis_forcing/global/v20260514/global_pet_hargreaves_samani_0p1_v20260514.nc"   
 
-#~ # on snellius
-#~ PCRGLOBWB_MODEL_SCRIPT_FOLDER="/home/edwin/github/edwinkost/PCR-GLOBWB_model/model/"
+#~ # get the aguments
+#~ INI_FILE=$1                          
+#~ MAIN_OUTPUT_DIRECTORY=$2             
+#~ STARTING_DATE=$3                     
+#~ END_DATE=$4                          
+#~ NUM_OF_YEARS_FOR_SPINUP=$5        
+#~ MAIN_INITIAL_STATE_FOLDER=$6         
+#~ DATE_FOR_INITIAL_STATES=$7           
+#~ MAIN_INPUT_DIRECTORY=$8
+#~ PRECIPITATION_FORCING_FILE=$8        
+#~ TEMPERATURE_FORCING_FILE=$9          
+#~ REF_POT_ET_FORCING_FILE=${10}           
 
 # on eejit
 PCRGLOBWB_MODEL_SCRIPT_FOLDER="/eejit/home/sutan101/github/edwinkost/PCR-GLOBWB_model/model/"
+
+#~ # on snellius
+#~ PCRGLOBWB_MODEL_SCRIPT_FOLDER="/home/edwin/github/edwinkost/PCR-GLOBWB_model/model/"
 
 # load modules on eejit
 . /eejit/home/sutan10/load_default.sh
@@ -40,8 +52,8 @@ PCRGLOBWB_MODEL_SCRIPT_FOLDER="/eejit/home/sutan101/github/edwinkost/PCR-GLOBWB_
 # - unset pcraster working threads
 unset PCRASTER_NR_WORKER_THREADS
 
-#~ # - you may have to activate the following
-#~ export OPENBLAS_NUM_THREADS=1
+# - you may have to activate the following
+export OPENBLAS_NUM_THREADS=1
 
 # go to the folder that contain PCR-GLOBWB scripts
 cd ${PCRGLOBWB_MODEL_SCRIPT_FOLDER}
@@ -59,18 +71,17 @@ do
 
 CLONE_CODE=${i}
 python3 deterministic_runner_ulysses.py ${INI_FILE} debug_parallel ${CLONE_CODE} \
--mod          ${MAIN_OUTPUT_DIRECTORY} \
--sd           ${STARTING_DATE} \
--ed           ${END_DATE} \
--noyfsu       ${NUM_OF_YEARS_FOR_SPINUP} \
--pff          ${PRECIPITATION_FORCING_FILE} \
--tff          ${TEMPERATURE_FORCING_FILE} \
--rpetff       ${REF_POT_ET_FORCING_FILE} \
--misf         ${MAIN_INITIAL_STATE_FOLDER} \
--dfis         ${DATE_FOR_INITIAL_STATES} \
--bfexp        ${BASEFLOW_EXPONENT} \
--log10mfksat  ${LOG_10_MULTIPLIER_FOR_KSAT} \
--log10mfreccf ${LOG_10_MULTIPLIER_FOR_RECESSION_COEFF} \
+-mod     ${MAIN_OUTPUT_DIRECTORY}      \
+-mid     ${MAIN_INPUT_DIRECTORY}       \
+-sd      ${STARTING_DATE}              \
+-ed      ${END_DATE}                   \
+-noyfsu  ${NUM_OF_YEARS_FOR_SPINUP}    \
+-pff     ${PRECIPITATION_FORCING_FILE} \
+-tff     ${TEMPERATURE_FORCING_FILE}   \
+-rpetff  ${REF_POT_ET_FORCING_FILE}    \
+-misf    ${MAIN_INITIAL_STATE_FOLDER}  \
+-dfis    ${DATE_FOR_INITIAL_STATES}    \
+-end_of_arguments
 &
 
 done
@@ -78,18 +89,17 @@ done
 
 # merging process
 python3 dynamic_file_merging_ulysses.py ${INI_FILE} \
--mod          ${MAIN_OUTPUT_DIRECTORY} \
--sd           ${STARTING_DATE} \
--ed           ${END_DATE} \
--noyfsu       ${NUM_OF_YEARS_FOR_SPINUP} \
--pff          ${PRECIPITATION_FORCING_FILE} \
--tff          ${TEMPERATURE_FORCING_FILE} \
--rpetff       ${REF_POT_ET_FORCING_FILE} \
--misf         ${MAIN_INITIAL_STATE_FOLDER} \
--dfis         ${DATE_FOR_INITIAL_STATES} \
--bfexp        ${BASEFLOW_EXPONENT} \
--log10mfksat  ${LOG_10_MULTIPLIER_FOR_KSAT} \
--log10mfreccf ${LOG_10_MULTIPLIER_FOR_RECESSION_COEFF} \
+-mod     ${MAIN_OUTPUT_DIRECTORY}      \
+-mid     ${MAIN_INPUT_DIRECTORY}       \
+-sd      ${STARTING_DATE}              \
+-ed      ${END_DATE}                   \
+-noyfsu  ${NUM_OF_YEARS_FOR_SPINUP}    \
+-pff     ${PRECIPITATION_FORCING_FILE} \
+-tff     ${TEMPERATURE_FORCING_FILE}   \
+-rpetff  ${REF_POT_ET_FORCING_FILE}    \
+-misf    ${MAIN_INITIAL_STATE_FOLDER}  \
+-dfis    ${DATE_FOR_INITIAL_STATES}    \
+-end_of_arguments
 &
 
 wait
