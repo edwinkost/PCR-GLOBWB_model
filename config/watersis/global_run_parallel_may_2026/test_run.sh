07c7466304b636fd046d728ac6eeefe379b1c86d
U-SOLISCOM\Sutan101 <Sutan101@UU105378.soliscom.uu.nl>
2026-06-03 12:17:58 +0200

    working on the parallelization of the global run

--- config/watersis/global_run_parallel_may_2026/test_run.sh
+++ config/watersis/global_run_parallel_may_2026/test_run.sh
@@ -21,8 +21,8 @@ cd /eejit/home/sutan101/github/edwinkost/PCR-GLOBWB_model/model/
 
 CLONE_CODE="2"
 
-python3 deterministic_runner_ulysses.py ${INI_FILE} debug_parallel ${CLONE_CODE} \
 #~ python3 deterministic_runner_ulysses.py ${INI_FILE} debug \
+python3 deterministic_runner_ulysses.py ${INI_FILE} debug_parallel ${CLONE_CODE} \
 -mod     ${MAIN_OUTPUT_DIRECTORY}      \
 -mid     ${MAIN_INPUT_DIRECTORY}       \
 -sd      ${STARTING_DATE}              \
