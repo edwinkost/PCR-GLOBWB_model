c1b569bcb40527312a9ce9cd5a79ffc7fd9396d8
U-SOLISCOM\Sutan101 <Sutan101@UU105378.soliscom.uu.nl>
2026-07-09 15:30:11 +0200

    start testing for the ATOS

--- config/watersis/config_watersis_2026_july/pcrglobwb_run_test.sh
+++ config/watersis/config_watersis_2026_july/pcrglobwb_run_test.sh
@@ -52,11 +52,11 @@ module load gdal/3.6.2
 #~ # load modules on snellius
 #~ . /home/edwin/load_all_default.sh
 
-# - unset pcraster working threads
-unset PCRASTER_NR_WORKER_THREADS
+#~ # - unset pcraster working threads
+#~ unset PCRASTER_NR_WORKER_THREADS
 
-# - you may have to activate the following
-export OPENBLAS_NUM_THREADS=1
+#~ # - you may have to activate the following
+#~ export OPENBLAS_NUM_THREADS=1
 
 # go to the folder that contain PCR-GLOBWB scripts
 cd ${PCRGLOBWB_MODEL_SCRIPT_FOLDER}
