f9c3f7a11fd03d1914f83596aa460f3781471ef1
U-SOLISCOM\Sutan101 <Sutan101@UU105378.soliscom.uu.nl>
2026-07-09 15:43:50 +0200

    start testing for the ATOS

--- config/watersis/config_watersis_2026_july/kdj_mem_monit.sh
+++ config/watersis/config_watersis_2026_july/kdj_mem_monit.sh
@@ -1,4 +1,8 @@
+
 command="my_command"
+
+command="python3 deterministic_runner_ulysses.py ../config/watersis/config_watersis_2026_july/setup_6min_global_watersis_nopar_develop.ini debug -mod /lus/h2resw01/fws4/sb/project/C3SHydroGL/edwin/test_global_6min/ -mid /ec/fws4/sb/project/C3SHydroGL/edwin/data/pcrglobwb_input_watersis/develop/global_6min/ -sd 2000-01-01 -ed 2000-01-31 -noyfsu 1 -pff /ec/fws4/sb/project/C3SHydroGL/edwin/data/watersis_forcing/global/v20260514/global_pre_0p1_v20260514.nc -tff /ec/fws4/sb/project/C3SHydroGL/edwin/data/watersis_forcing/global/v20260514/global_tas_day_0p1_v20260514.nc -rpetff /ec/fws4/sb/project/C3SHydroGL/edwin/data/watersis_forcing/global/v20260514/global_pet_hargreaves_samani_0p1_v20260514.nc -misf dummy_initial_conditions/ -dfis 1999-12-31 -end"
+
 sleep_duration=5 # Seconds
 
 while true; do
