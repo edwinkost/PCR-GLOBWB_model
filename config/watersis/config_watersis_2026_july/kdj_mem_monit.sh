a9bb2a4887c5ea29977602f4bf314b4381ede7cd
U-SOLISCOM\Sutan101 <Sutan101@UU105378.soliscom.uu.nl>
2026-07-10 13:27:41 +0200

    testing on ATOS

--- config/watersis/config_watersis_2026_july/kdj_mem_monit.sh
+++ config/watersis/config_watersis_2026_july/kdj_mem_monit.sh
@@ -1,7 +1,7 @@
 
 command="my_command"
 
-command="python3 deterministic_runner_ulysses.py ../config/watersis/config_watersis_2026_july/setup_6min_global_watersis_nopar_develop.ini debug -mod /lus/h2resw01/fws4/sb/project/C3SHydroGL/edwin/test_pcrglobwb_global_6min/with_mk_forcing_without_workers/ -mid /ec/fws4/sb/project/C3SHydroGL/edwin/data/pcrglobwb_input_watersis/develop/global_6min/ -sd 2000-01-01 -ed 2000-01-31 -noyfsu 0 -pff /ec/fws4/sb/project/C3SHydroGL/phase_3/global/reanalysis/meteo/era5_em_earth/2000/01/pre_0p1.nc -tff /ec/fws4/sb/project/C3SHydroGL/phase_3/global/reanalysis/meteo/era5_em_earth/2000/01/tas_day.nc -rpetff /ec/fws4/sb/project/C3SHydroGL/phase_3/global/reanalysis/meteo/era5_em_earth/2000/01/pet_hargreaves_samani_0p1.nc -misf dummy_initial_conditions/ -dfis 1999-12-31 -end"
+command="python3 deterministic_runner_ulysses.py ../config/watersis/config_watersis_2026_july/setup_6min_global_watersis_nopar_develop.ini debug -mod /lus/h2resw01/fws4/sb/project/C3SHydroGL/edwin/test_pcrglobwb_global_6min/with_mk_forcing_with_workers/ -mid /ec/fws4/sb/project/C3SHydroGL/edwin/data/pcrglobwb_input_watersis/develop/global_6min/ -sd 2000-01-01 -ed 2000-01-31 -noyfsu 0 -pff /ec/fws4/sb/project/C3SHydroGL/phase_3/global/reanalysis/meteo/era5_em_earth/2000/01/pre_0p1.nc -tff /ec/fws4/sb/project/C3SHydroGL/phase_3/global/reanalysis/meteo/era5_em_earth/2000/01/tas_day.nc -rpetff /ec/fws4/sb/project/C3SHydroGL/phase_3/global/reanalysis/meteo/era5_em_earth/2000/01/pet_hargreaves_samani_0p1.nc -misf dummy_initial_conditions/ -dfis 1999-12-31 -end"
 
 sleep_duration=5 # Seconds
 
