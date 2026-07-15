32e345a9632a35f6ac347771890fd92b1ba3eb75
U-SOLISCOM\Sutan101 <Sutan101@UU105378.soliscom.uu.nl>
2026-07-15 12:45:02 +0200

    Start a test run for Europe

--- config/watersis/config_watersis_2026_july/global_6min/run_pcrglobwb_global_6min.sh
+++ config/watersis/config_watersis_2026_july/global_6min/run_pcrglobwb_global_6min.sh
@@ -21,8 +21,8 @@ module load pcraster/4.4.0-01
 module load gdal/3.6.2
 
 # to speed up, using some cores/threads
-export OMP_NUM_THREADS=8
-export PCRASTER_NR_WORKER_THREADS=8
+export OMP_NUM_THREADS=16
+export PCRASTER_NR_WORKER_THREADS=16
 
 #~ # activate the following for using a single core/thread (lo
 #~ # - unset pcraster working threads
