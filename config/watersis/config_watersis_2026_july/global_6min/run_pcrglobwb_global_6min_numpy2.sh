4a024d08270803a298c646609d7d6add6d23ee59
root <root@puppypc12763.edwin>
2026-08-15 02:30:26 +0200

    to support numpy 2.0

--- config/watersis/config_watersis_2026_july/global_6min/run_pcrglobwb_global_6min_numpy2.sh
+++ config/watersis/config_watersis_2026_july/global_6min/run_pcrglobwb_global_6min_numpy2.sh
@@ -4,8 +4,7 @@
 #SBATCH --job-name=pgb_6min_global
 
 #SBATCH --mem-per-cpu=1250MB
-#~ #SBATCH --ntasks=16
-#SBATCH --ntasks=32
+#SBATCH --ntasks=16
 #SBATCH --cpus-per-task=1
 #SBATCH --threads-per-core=2
 
@@ -21,15 +20,14 @@ set -x
 #~ module load python3/3.10.10-01 
 #~ module load pcraster/4.4.0-01 
 #~ module load gdal/3.6.2
-# - using pcraster 4.4.2 (and numpy 2.0)
+# - using pcraster 4.4.2 (and numpy 2.0) - NOT working if we set PCRASTER_NR_WORKER_THREADS
 module load python3/3.12.9-01
 module load pcraster/4.4.2-01
 module load gdal/3.10.2
 
 # to speed up, using some cores/threads
-export PCRASTER_NR_WORKER_THREADS=4
-export OMP_NUM_THREADS=4
-export OPENBLAS_NUM_THREADS=4
+#~ export PCRASTER_NR_WORKER_THREADS=16
+export OMP_NUM_THREADS=16
 
 #~ # activate the following for using a single core/thread (lo
 #~ # - unset pcraster working threads
