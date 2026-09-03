3ec1e223db6d0f571af9986f200f0199d60f1d59
root <root@puppypc12763.edwin>
2026-09-03 14:55:23 +0200

    start again the global 6min run in parallel

---
+++ config/watersis/global_run_parallel_202609XX/complete_job_scripts_for_a_global_run.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+
+JOBNAME="pgb_6min"
+#~ JOBNAME=$1
+
+set -x
+
+MAIN_OUTPUT_DIR="/scratch/depfg/sutan101/watersis_runs_202609XX/global_6min_with_watersis_forcing_with_parallel_v20260903/"${JOBNAME}"/"
+
+PCRGLOBWB_MODEL_SCRIPT_FOLDER="/eejit/home/sutan101/github/edwinkost/PCR-GLOBWB_model/model/"
+
+SPINUP_RUN_INI="/eejit/home/sutan101/github/edwinkost/PCR-GLOBWB_model/config/watersis/global_run_parallel_202609XX/setup_6min_global_watersis_develop_since_sep_2026.ini"
+WARMED_RUN_INI="/eejit/home/sutan101/github/edwinkost/PCR-GLOBWB_model/config/watersis/global_run_parallel_202609XX/setup_6min_global_watersis_develop_since_sep_2026.ini" 
+
+MAIN_INPUT_DIRECTORY="/scratch/depfg/sutan101_new/pcrglobwb_input_watersis/develop/global_6min/"   
+
+PRECIPITATION_FORCING_FILE="/scratch/depfg/sutan101/data/watersis_forcing/global/v20260813/global_pre_0p1_v20260813.nc"
+TEMPERATURE_FORCING_FILE="/scratch/depfg/sutan101/data/watersis_forcing/global/v20260813/global_tas_day_0p1_v20260813.nc"  
+REF_POT_ET_FORCING_FILE="/scratch/depfg/sutan101/data/watersis_forcing/global/v20260813/global_pet_hargreaves_samani_0p1_v20260813.nc"   
+
+#~ sutan101@node044.cluster:/scratch/depfg/sutan101/data/watersis_forcing/global/v20260813$ ls -lah
+#~ total 307G
+#~ drwxr-xr-x 2 sutan101 depfg    7 Aug 13 13:30 .
+#~ drwxr-xr-x 4 sutan101 depfg    4 Aug 13 05:39 ..
+#~ -rw-r--r-- 1 sutan101 depfg 353G Aug 13 08:03 global_pet_hargreaves_samani_0p1_v20260813.nc
+#~ -rw-r--r-- 1 sutan101 depfg 353G Aug 13 10:53 global_pre_0p1_v20260813.nc
+#~ -rw-r--r-- 1 sutan101 depfg 353G Aug 13 13:30 global_tas_day_0p1_v20260813.nc
+#~ -rw-r--r-- 1 sutan101 depfg  571 Aug 13 13:30 merging_global_forcing.sh
+#~ -rw-r--r-- 1 sutan101 depfg  123 Aug 13 13:30 rsync_to_eejit.sh
+
+# spin up run
+NUM_OF_YEARS_FOR_SPINUP="25"
+SUB_JOBNAME=${JOBNAME}_spinup_with_1970
+SUB_INIFILE=${SPINUP_RUN_INI}
+STA_DATE="1970-01-01"
+END_DATE="1970-12-31"
+INITIAL_FOLD="dummy_initial_conditions/"
+INITIAL_DATE="1999-12-31"
+SUB_OUT_DIR=${MAIN_OUTPUT_DIR}/_spinup/with_1970/
+
+#~ # - for testing
+#~ NUM_OF_YEARS_FOR_SPINUP="0"
+#~ STA_DATE="1970-12-29"
+
+#~ # - for testing with 5 year spinup
+#~ NUM_OF_YEARS_FOR_SPINUP="5"
+#~ STA_DATE="1970-01-01"
+
+# - start the run
+SPINUP=$(sbatch -J "${SUB_JOBNAME}" --export INI_FILE="${SUB_INIFILE}",MAIN_OUTPUT_DIR="${SUB_OUT_DIR}",STARTING_DATE="${STA_DATE}",END_DATE="${END_DATE}",MAIN_INITIAL_STATE_FOLDER="${INITIAL_FOLD}",DATE_FOR_INITIAL_STATES="${INITIAL_DATE}",NUM_OF_YEARS_FOR_SPINUP="${NUM_OF_YEARS_FOR_SPINUP}",MAIN_INPUT_DIRECTORY="${MAIN_INPUT_DIRECTORY}",PRECIPITATION_FORCING_FILE=${PRECIPITATION_FORCING_FILE},TEMPERATURE_FORCING_FILE=${TEMPERATURE_FORCING_FILE},REF_POT_ET_FORCING_FILE=${REF_POT_ET_FORCING_FILE},PCRGLOBWB_MODEL_SCRIPT_FOLDER=${PCRGLOBWB_MODEL_SCRIPT_FOLDER} job_script_sbatch_pcrglobwb_template.sh | sed 's/Submitted batch job //')
+
+
+# run for the period 1970-2019
+SUB_JOBNAME=${JOBNAME}_1970-2019
+SUB_INIFILE=${WARMED_RUN_INI}
+STA_DATE="1970-01-01"
+END_DATE="2019-12-31"
+INITIAL_FOLD=${SUB_OUT_DIR}/global/states/
+INITIAL_DATE="1970-12-31"
+SUB_OUT_DIR=${MAIN_OUTPUT_DIR}/begin_from_1970/
+# - start the run
+FIRST=$(sbatch --dependency=afterany:${SPINUP} -J "${SUB_JOBNAME}" --export INI_FILE="${SUB_INIFILE}",MAIN_OUTPUT_DIR="${SUB_OUT_DIR}",STARTING_DATE="${STA_DATE}",END_DATE="${END_DATE}",MAIN_INITIAL_STATE_FOLDER="${INITIAL_FOLD}",DATE_FOR_INITIAL_STATES="${INITIAL_DATE}",NUM_OF_YEARS_FOR_SPINUP="${NUM_OF_YEARS_FOR_SPINUP}",MAIN_INPUT_DIRECTORY="${MAIN_INPUT_DIRECTORY}",PRECIPITATION_FORCING_FILE=${PRECIPITATION_FORCING_FILE},TEMPERATURE_FORCING_FILE=${TEMPERATURE_FORCING_FILE},REF_POT_ET_FORCING_FILE=${REF_POT_ET_FORCING_FILE},PCRGLOBWB_MODEL_SCRIPT_FOLDER=${PCRGLOBWB_MODEL_SCRIPT_FOLDER} job_script_sbatch_pcrglobwb_template.sh | sed 's/Submitted batch job //')
+set +x
+
+
+echo $SPINUP
+echo $FIRST
+
+sleep 3
+squeue
+squeue -u sutan101
+
+
+
---
+++ config/watersis/global_run_parallel_202609XX/job_script_sbatch_pcrglobwb_template.sh
@@ -0,0 +1,69 @@
+#!/bin/bash 
+#SBATCH -N 1
+
+#~ # on snellius
+#~ #SBATCH -n 96
+#~ #SBATCH -p genoa
+#~ #SBATCH -t 119:59:00
+#~ #SBATCH -t 59:00
+
+# on eejit
+#SBATCH -n 96
+#SBATCH -p defq
+#SBATCH --exclusive
+
+#SBATCH -J pgb_watersis_spinup_and_actual_runs
+
+# mail alert at start, end and abortion of execution
+#SBATCH --mail-type=ALL
+
+# send mail to this address
+#SBATCH --mail-user=edwinkost@gmail.com
+
+#~ #SBATCH --export INI_FILE=setup_6arcmin.ini,MAIN_OUTPUT_DIR="/scratch/pcrglobwb_ulysses_reference_runs/test/",STARTING_DATE="1981-01-01",END_DATE="2000-12-31",MAIN_INITIAL_STATE_FOLDER="/scratch/spinup/global/states/",DATE_FOR_INITIAL_STATES="1981-12-31",BASEFLOW_EXPONENT="1.0",LOG_10_MULTIPLIER_FOR_KSAT="0.0",LOG_10_MULTIPLIER_FOR_RECESSION_COEFF=0.0,NUM_OF_YEARS_FOR_SPINUP="0.0"
+
+#SBATCH --export INI_FILE="/eejit/home/sutan101/github/edwinkost/PCR-GLOBWB_model/config/watersis/global_run_parallel_may_2026/setup_6min_global_watersis_develop.ini",MAIN_OUTPUT_DIR="/scratch/pcrglobwb_ulysses_reference_runs/test/",STARTING_DATE="1981-01-01",END_DATE="2000-12-31",MAIN_INITIAL_STATE_FOLDER="/scratch/spinup/global/states/",DATE_FOR_INITIAL_STATES="1981-12-31",NUM_OF_YEARS_FOR_SPINUP="5",MAIN_INPUT_DIRECTORY="/INPUT_FOLDER/",PRECIPITATION_FORCING_FILE="/pre.nc",TEMPERATURE_FORCING_FILE="/tavg.nc",REF_POT_ET_FORCING_FILE="/pet0.nc",PCRGLOBWB_MODEL_SCRIPT_FOLDER="/eejit/home/sutan101/github/edwinkost/PCR-GLOBWB_model/model/"
+
+
+set -x
+
+# set the configuration file (*.ini)
+INI_FILE=${INI_FILE}
+
+# set the output folder
+MAIN_OUTPUT_DIR=${MAIN_OUTPUT_DIR}
+
+# set the input folder
+MAIN_INPUT_DIRECTORY=${MAIN_INPUT_DIRECTORY}
+
+# set the starting and end simulation dates
+STARTING_DATE=${STARTING_DATE}
+END_DATE=${END_DATE}
+
+# set the initial conditions (folder and time stamp for the files)
+MAIN_INITIAL_STATE_FOLDER=${MAIN_INITIAL_STATE_FOLDER}
+DATE_FOR_INITIAL_STATES=${DATE_FOR_INITIAL_STATES}
+
+# set the forcing files
+#~ PRECIPITATION_FORCING_FILE="NONE"
+#~ TEMPERATURE_FORCING_FILE="NONE"
+#~ REF_POT_ET_FORCING_FILE="NONE"
+PRECIPITATION_FORCING_FILE="${PRECIPITATION_FORCING_FILE}"
+TEMPERATURE_FORCING_FILE="${TEMPERATURE_FORCING_FILE}"
+REF_POT_ET_FORCING_FILE="${REF_POT_ET_FORCING_FILE}"
+
+
+# go to the folder that contain the bash script that will call parallel runs (e.g. pcrglobwb_runs_71_clones.sh)
+# - assuming it will be located in the folder that contains INI_FILE 
+DIR="$(dirname "${INI_FILE}")"
+cd ${DIR}
+
+# make the run for every clone 
+bash pcrglobwb_runs_71_clones.sh ${INI_FILE} ${MAIN_OUTPUT_DIR} ${STARTING_DATE} ${END_DATE} ${NUM_OF_YEARS_FOR_SPINUP} ${MAIN_INITIAL_STATE_FOLDER} ${DATE_FOR_INITIAL_STATES} ${MAIN_INPUT_DIRECTORY} ${PRECIPITATION_FORCING_FILE} ${TEMPERATURE_FORCING_FILE} ${REF_POT_ET_FORCING_FILE} ${PCRGLOBWB_MODEL_SCRIPT_FOLDER}
+
+# wait for 30 sec 
+sleep 30
+
+set +x
+
+exit
