d745e55265265ba87ddb2a103a9bca853e2006cc
Edwin Sutanudjaja <edwinkost@heol>
2026-09-15 23:59:24 +0200

    rerun for mswep

--- config/ulysses/develop_for_rerun_on_202609XX/0_call_mswep_run.sh
+++ config/ulysses/develop_for_rerun_on_202609XX/0_call_mswep_run.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-bash complete_job_scripts_for_global_run.sh "mswep_pgb_uly_rerun_two_lcs_sqrt" "SQUARE_ROOT"
+bash complete_job_scripts_for_global_run_mswep.sh "mswep_pgb_uly_rerun_two_lcs_sqrt" "SQUARE_ROOT"
 #~ bash complete_job_scripts_for_global_run.sh "orig_2lcs" "0.0"
 
 set +x
