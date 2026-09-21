d858031196530df961d65a77f0da6565d3c79846
Edwin Sutanudjaja <edwinkost@heol>
2026-09-21 22:59:02 +0200

    start mswep on snellius

--- config/ulysses/develop_for_rerun_on_202609XX/complete_job_scripts_for_global_run_mswep.sh
+++ config/ulysses/develop_for_rerun_on_202609XX/complete_job_scripts_for_global_run_mswep.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-JOBNAME="mswep_pgb_uly_rerun_two_lcs_sqrt"
-JOBNAME="mswep_pgb_uly_rerun_two_lcs_sqrt_RERUN"
+JOBNAME="emearth_pgb_uly_rerun_two_lcs_sqrt"
 LOGRECF="SQUARE_ROOT"
 
 JOBNAME=$1
@@ -13,7 +12,10 @@ LOG_10_MULTIPLIER_FOR_KSAT="0.0"
 SPINUP_RUN_INI="setup_6arcmin_ulysses_2LCs_version_2023-12-14_mswep.ini"
 WARMED_RUN_INI="setup_6arcmin_ulysses_2LCs_version_2023-12-14_mswep.ini" 
 
-MAIN_OUTPUT_DIR="/scratch/depfg/sutan101/pcrglobwb_ulysses_2023-12-XX_rerun_on_202609XX/mswep/"${JOBNAME}
+#~ MAIN_OUTPUT_DIR="/scratch/depfg/sutan101/pcrglobwb_ulysses_2023-12-XX_rerun_on_202609XX/mswep/"${JOBNAME}
+
+# - on snellius
+MAIN_OUTPUT_DIR="/scratch-shared/edwin/pcrglobwb_ulysses_2023-12-XX_rerun_on_202609/mswep/"${JOBNAME}
 
 set -x
 
@@ -23,7 +25,9 @@ SUB_JOBNAME=${JOBNAME}_spinup_with_1981
 SUB_INIFILE=${SPINUP_RUN_INI}
 STA_DATE="1981-01-01"
 END_DATE="1981-12-31"
-INITIAL_FOLD="/scratch/depfg/sutan101/data/pcrglobwb_input_ulysses/initial_conditions/from_runs_created_in_january_2021/"
+#~ INITIAL_FOLD="/scratch/depfg/sutan101/data/pcrglobwb_input_ulysses/initial_conditions/from_runs_created_in_january_2021/"
+# - on snellius
+INITIAL_FOLD="/scratch-shared/edwin/data/pcrglobwb_input_ulysses/initial_conditions/from_runs_created_in_january_2021/"
 INITIAL_DATE="1981-12-31"
 SUB_OUT_DIR=${MAIN_OUTPUT_DIR}/_spinup/with_1981/
 
@@ -40,7 +44,7 @@ SPINUP=$(sbatch -J "${SUB_JOBNAME}" --export INI_FILE="${SUB_INIFILE}",MAIN_OUTP
 
 
 # run for the period 1981-2019
-NUM_OF_YEARS_FOR_SPINUP="0"
+NUM_OF_YEARS_FOR_SPINUP="5"
 SUB_JOBNAME=${JOBNAME}_1981-2019
 SUB_INIFILE=${WARMED_RUN_INI}
 STA_DATE="1981-01-01"
@@ -60,5 +64,5 @@ echo $FIRST
 
 sleep 3
 squeue
-squeue -u sutan101
+#~ squeue -u sutan101
 
