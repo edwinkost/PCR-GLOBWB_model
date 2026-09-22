266d96e84a3f26ceb7e05ef8726e7c1fd2005763
root <root@puppylinux_vgen>
2026-09-22 23:18:55 +0200

    start testing on Atos

--- config/watersis/config_watersis_202609/run_pcrglobwb_global_6min_on_atos.sh
+++ config/watersis/config_watersis_202609/run_pcrglobwb_global_6min_on_atos.sh
@@ -17,10 +17,10 @@
 #~ module load pcraster/4.4.0-01 
 #~ module load gdal/3.6.2
 
-# load modules on Atos - using pcraster 4.4.1-01 (may24)
-module load python3/3.11.8-01
-module load pcraster/4.4.1-01 
-module load gdal/3.8.4
+#~ # load modules on Atos - using pcraster 4.4.1-01 (may24) - THE FOLLOWING DOES NOT WORK
+#~ module load python3/3.11.8-01
+#~ module load pcraster/4.4.1-01 
+#~ module load gdal/3.8.4
 
 # to speed up, using some cores/threads
 export OMP_NUM_THREADS=16
@@ -32,8 +32,8 @@ DIR_INI_FILE=$(pwd)
 INI_FILE=${DIR_INI_FILE}/${INI_FILE}
 
 # Output directory  
-#~ MAIN_OUTPUT_DIRECTORY="/lus/h2resw01/fws4/sb/project/C3SHydroGL/edwin/test_pgb_6min_202609/test/"     
-MAIN_OUTPUT_DIRECTORY="/lus/h2resw01/fws4/sb/project/C3SHydroGL/edwin/test_pgb_6min_202609/test_with_pcraster_4.4.1/"     
+MAIN_OUTPUT_DIRECTORY="/lus/h2resw01/fws4/sb/project/C3SHydroGL/edwin/test_pgb_6min_202609/test/"     
+#~ MAIN_OUTPUT_DIRECTORY="/lus/h2resw01/fws4/sb/project/C3SHydroGL/edwin/test_pgb_6min_202609/test_with_pcraster_4.4.1/"     
 
 # Model input directory (containing model parameters)
 #~ MAIN_INPUT_DIRECTORY="/scratch/depfg/sutan101/data/pcrglobwb_input_watersis/develop/global_6min/"   
