ba79bb72e7f7e7739e1a2b47ac1cbaaea83734bc
root <root@puppylinux_vgen>
2026-09-22 22:30:12 +0200

    start testing on Atos

---
+++ config/watersis/config_watersis_202609/run_pcrglobwb_global_6min_on_atos.sh
@@ -0,0 +1,107 @@
+#!/bin/bash 
+
+#SBATCH --qos=nf
+#SBATCH --job-name=pgb_6min_global
+
+#SBATCH --mem-per-cpu=1250MB
+#SBATCH --ntasks=16
+#SBATCH --cpus-per-task=1
+#SBATCH --threads-per-core=2
+
+#SBATCH --time=30:00
+# A one-month global 6min run (with this configuration) should take less than 15 mins.
+#~ #SBATCH --time=15:00
+
+# load modules on Atos - using pcraster 4.4.1-01 (may24)
+module load python3/3.11.8-01
+module load pcraster/4.4.1-01 
+module load gdal/3.8.4
+
+#~ # load modules on Atos - the following works (and it was used by Robert UFZ)
+#~ module load python3/3.10.10-01 
+#~ module load pcraster/4.4.0-01 
+#~ module load gdal/3.6.2
+
+# to speed up, using some cores/threads
+export OMP_NUM_THREADS=16
+export PCRASTER_NR_WORKER_THREADS=16
+
+# PCR-GLOBWB configuration (.ini) file that will be used  
+INI_FILE="setup_6min_global_watersis_v202609_develop.ini"
+DIR_INI_FILE=$(pwd)
+INI_FILE=${DIR_INI_FILE}/${INI_FILE}
+
+# Output directory  
+MAIN_OUTPUT_DIRECTORY="/lus/h2resw01/fws4/sb/project/C3SHydroGL/edwin/test_pgb_6min_202609/test/"     
+
+# Model input directory (containing model parameters)
+#~ MAIN_INPUT_DIRECTORY="/scratch/depfg/sutan101/data/pcrglobwb_input_watersis/develop/global_6min/"   
+MAIN_INPUT_DIRECTORY="/home/cyes/C3SHydroGL/edwin/data/pcrglobwb_input_watersis/release/global_6min_v20260922/"   
+
+# Starting and end date.  
+STARTING_DATE="1981-01-01"             
+END_DATE="1981-01-31"                  
+
+# Number of spin up years (this should be zero, as it is assumed that warm initial conditions are provided). 
+NUM_OF_YEARS_FOR_SPINUP=0   
+
+# Initial conditions, folder and the date
+#~ MAIN_INITIAL_STATE_FOLDER="/scratch/depfg/sutan101/data/pcrglobwb_input_watersis/develop/global_6min/initial_conditions/from_global_6min_run_v20260904/" 
+#~ MAIN_INITIAL_STATE_FOLDER="initial_conditions/from_global_6min_run_v20260904/"
+MAIN_INITIAL_STATE_FOLDER="/home/cyes/C3SHydroGL/edwin/data/pcrglobwb_input_watersis/release/global_6min_v20260922/initial_conditions/from_global_6min_run_v20260904/" 
+DATE_FOR_INITIAL_STATES="1980-12-31"   
+
+
+# Forcing input directory
+#~ PRECIPITATION_FORCING_FILE="/scratch/depfg/sutan101/data/watersis_forcing/global/v20260813/global_pre_0p1_v20260813.nc"
+#~ TEMPERATURE_FORCING_FILE="/scratch/depfg/sutan101/data/watersis_forcing/global/v20260813/global_tas_day_0p1_v20260813.nc"  
+#~ REF_POT_ET_FORCING_FILE="/scratch/depfg/sutan101/data/watersis_forcing/global/v20260813/global_pet_hargreaves_samani_0p1_v20260813.nc"   
+#~ PRECIPITATION_FORCING_FILE="/ec/fws4/sb/project/C3SHydroGL/phase_3/global/reanalysis/meteo/era5_em_earth/2000/01/pre_0p1.nc"
+#~ TEMPERATURE_FORCING_FILE="/ec/fws4/sb/project/C3SHydroGL/phase_3/global/reanalysis/meteo/era5_em_earth/2000/01/tas_day.nc"  
+#~ REF_POT_ET_FORCING_FILE="/ec/fws4/sb/project/C3SHydroGL/phase_3/global/reanalysis/meteo/era5_em_earth/2000/01/pet_hargreaves_samani_0p1.nc"  
+PRECIPITATION_FORCING_FILE="/ec/fws4/sb/project/C3SHydroGL/phase_3/global/reanalysis/meteo/era5/mem_00/1980/01/pr_era5_mem_00_1980-01-01_1980-01-31_daily.nc"
+TEMPERATURE_FORCING_FILE="/ec/fws4/sb/project/C3SHydroGL/phase_3/global/reanalysis/meteo/era5/mem_00/1980/01/tas_era5_mem_00_1980-01-01_1980-01-31_daily.nc"  
+REF_POT_ET_FORCING_FILE="/ec/fws4/sb/project/C3SHydroGL/phase_3/global/reanalysis/meteo/era5/mem_00/1980/01/pet_era5_mem_00_1980-01-01_1980-01-31_daily.nc"  
+
+#~ cyes@ac6-194.bullx:/ec/fws4/sb/project/C3SHydroGL/phase_3/global/reanalysis/meteo/era5/mem_00/1980/01$ ls -lah
+#~ total 1.2G
+#~ dr-xr-sr-x+  2 nemk c3hygl 4.0K Aug 27 15:30 .
+#~ dr-xr-sr-x+ 14 nemk c3hygl 4.0K Aug 27 15:41 ..
+#~ -r-xr-xr-x+  1 nemk c3hygl 124M Aug 27 15:30 hurs_era5_mem_00_1980-01-01_1980-01-31_daily.nc
+#~ -r-xr-xr-x+  1 nemk c3hygl 100M Aug 27 15:30 pet_era5_mem_00_1980-01-01_1980-01-31_daily.nc
+#~ -r-xr-xr-x+  1 nemk c3hygl 123M Aug 27 15:30 pr_era5_mem_00_1980-01-01_1980-01-31_daily.nc
+#~ -r-xr-xr-x+  1 nemk c3hygl 119M Aug 27 15:30 ps_era5_mem_00_1980-01-01_1980-01-31_daily.nc
+#~ -r-xr-xr-x+  1 nemk c3hygl 124M Aug 27 15:30 rlds_era5_mem_00_1980-01-01_1980-01-31_daily.nc
+#~ -r-xr-xr-x+  1 nemk c3hygl 111M Aug 27 15:30 rsds_era5_mem_00_1980-01-01_1980-01-31_daily.nc
+#~ -r-xr-xr-x+  1 nemk c3hygl 141M Aug 27 15:30 sfcWind_era5_mem_00_1980-01-01_1980-01-31_daily.nc
+#~ -r-xr-xr-x+  1 nemk c3hygl 109M Aug 27 15:30 tas_era5_mem_00_1980-01-01_1980-01-31_daily.nc
+#~ -r-xr-xr-x+  1 nemk c3hygl 109M Aug 27 15:30 tasmax_era5_mem_00_1980-01-01_1980-01-31_daily.nc
+#~ -r-xr-xr-x+  1 nemk c3hygl 110M Aug 27 15:30 tasmin_era5_mem_00_1980-01-01_1980-01-31_daily.nc
+
+# Directory containing the model script files
+#~ PCRGLOBWB_MODEL_SCRIPT_FOLDER="/home/cyes/github/edwinkost/PCR-GLOBWB_model/model/"
+PCRGLOBWB_MODEL_SCRIPT_FOLDER="/eejit/home/sutan101/gits/github/edwinkost/PCR-GLOBWB_model/model/"
+
+# go to the folder that contain PCR-GLOBWB model script file
+cd ${PCRGLOBWB_MODEL_SCRIPT_FOLDER}
+
+# run the model
+python3 deterministic_runner_ulysses.py ${INI_FILE} no-debug  \
+-mod     ${MAIN_OUTPUT_DIRECTORY}      \
+-mid     ${MAIN_INPUT_DIRECTORY}       \
+-sd      ${STARTING_DATE}              \
+-ed      ${END_DATE}                   \
+-noyfsu  ${NUM_OF_YEARS_FOR_SPINUP}    \
+-pff     ${PRECIPITATION_FORCING_FILE} \
+-tff     ${TEMPERATURE_FORCING_FILE}   \
+-rpetff  ${REF_POT_ET_FORCING_FILE}    \
+-misf    ${MAIN_INITIAL_STATE_FOLDER}  \
+-dfis    ${DATE_FOR_INITIAL_STATES}    \
+-end
+
+set +x
+
+exit
+
+
+
